8bc83ea6a4ad91a7d6c2a76a4e43c84c8e5e5461
bervalim <bernardogvalim@gmail.com>
2024-04-12 01:01:55 -0300

    registerForm-validators

--- ibs-sistemas-frontend/src/app/components/forms/register-form/register-form.component.ts
+++ ibs-sistemas-frontend/src/app/components/forms/register-form/register-form.component.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { RouterLink } from '@angular/router';
 
 @Component({
@@ -11,12 +16,31 @@ import { RouterLink } from '@angular/router';
 })
 export class RegisterFormComponent {
   registerForm = new FormGroup({
-    name: new FormControl(null),
-    email: new FormControl(null),
-    password: new FormControl(null),
-    sex: new FormControl(null),
-    birthDate: new FormControl(null),
-    civilState: new FormControl(null),
+    name: new FormControl(null, [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(80),
+    ]),
+    email: new FormControl(null, [
+      Validators.required,
+      Validators.email,
+      Validators.minLength(3),
+      Validators.maxLength(45),
+    ]),
+    password: new FormControl(null, [
+      Validators.required,
+      Validators.minLength(8),
+      Validators.pattern(/(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])/),
+    ]),
+    sex: new FormControl('', [Validators.required]),
+    birthDate: new FormControl(null, [
+      Validators.required,
+      Validators.maxLength(30),
+      Validators.pattern(
+        /(?:(?:31\/(?:0?[13578]|1[02]))\/|(?:(?:29|30)\/(?:0?[13-9]|1[0-2])\/))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29\/0?2\/(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])\/(?:(?:0?[1-9])|(?:1[0-2]))\/(?:(?:1[6-9]|[2-9]\d)?\d{2})/
+      ),
+    ]),
+    civilState: new FormControl('', [Validators.required]),
   });
 
   submit() {
