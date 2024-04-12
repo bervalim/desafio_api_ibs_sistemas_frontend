cea963b7f426558311716a502bd6bf969f311f3d
bervalim <bernardogvalim@gmail.com>
2024-04-11 23:13:26 -0300

    feat/register-formControl

--- ibs-sistemas-frontend/src/app/components/forms/register-form/register-form.component.ts
+++ ibs-sistemas-frontend/src/app/components/forms/register-form/register-form.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register-form',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './register-form.component.html',
-  styleUrl: './register-form.component.scss'
+  styleUrl: './register-form.component.scss',
 })
 export class RegisterFormComponent {
-
+  registerForm = new FormControl({
+    name: new FormControl(null),
+    email: new FormControl(null),
+    password: new FormControl(null),
+    sex: new FormControl(null),
+    birthDate: new FormControl(null),
+    civilState: new FormControl(null),
+  });
 }
