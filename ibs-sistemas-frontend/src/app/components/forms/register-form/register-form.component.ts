7755584d68dc7abacc52bcbae2092c2065e93f0c
bervalim <bernardogvalim@gmail.com>
2024-04-12 02:31:58 -0300

    feat/rendering-errors-register-form

--- ibs-sistemas-frontend/src/app/components/forms/register-form/register-form.component.ts
+++ ibs-sistemas-frontend/src/app/components/forms/register-form/register-form.component.ts
@@ -35,11 +35,22 @@ export class RegisterFormComponent {
     birthDate: new FormControl(null, [
       Validators.required,
       Validators.pattern(
-        /(?:(?:31\/(?:0?[13578]|1[02]))\/|(?:(?:29|30)\/(?:0?[13-9]|1[0-2])\/))(?:(?:1[6-9]|[2-9]\d)?\d{2})$|^(?:29\/0?2\/(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))$|^(?:0?[1-9]|1\d|2[0-8])\/(?:(?:0?[1-9])|(?:1[0-2]))\/(?:(?:1[6-9]|[2-9]\d)?\d{2})/
+        /(?:(?:31\/(?:0?[13578]|1[02]))\/|(?:(?:29|30)\/(?:0?[13-9]|1[0-2])\/))(?:1[6-9]\d{2}|[2-9]\d{3})$|^(?:29\/0?2\/(?:(?:(?:1[6-9]|[2-9]\d)\d{2})00|(?:(?:0[48]|[2468][048]|[13579][26])00)|(?:(?:0[2468]|[13579][26])00)|(?:[2468][048]|[13579][26]00)))$|^(?:0?[1-9]|1\d|2[0-8])\/(?:(?:0?[1-9])|(?:1[0-2]))\/(?:1[6-9]\d{2}|[2-9]\d{3})/
       ),
     ]),
     civilState: new FormControl('', [Validators.required]),
   });
+
+  get errors() {
+    return {
+      name: this.registerForm.get('name')?.errors,
+      email: this.registerForm.get('email')?.errors,
+      password: this.registerForm.get('password')?.errors,
+      sex: this.registerForm.get('sex')?.errors,
+      birthDate: this.registerForm.get('birthDate')?.errors,
+      civilState: this.registerForm.get('civilState')?.errors,
+    };
+  }
 
   submit() {
     const data = this.registerForm.value;
