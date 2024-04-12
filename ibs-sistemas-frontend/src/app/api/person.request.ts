af97f112e7b5f6d1e7f30a3d1c6b541909b5e5b7
bervalim <bernardogvalim@gmail.com>
2024-04-12 16:36:39 -0300

    feat/registerPeople-request

--- ibs-sistemas-frontend/src/app/api/person.request.ts
+++ ibs-sistemas-frontend/src/app/api/person.request.ts
@@ -1,10 +1,22 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import {
+  IRegisterPersonReturn,
+  IRegisterPersonReturnBirthday,
+  TRegisterBodyRequest,
+} from '../interfaces/person.interface';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PersonRequest {
   private BASE_URL = 'http://localhost:3000';
+
   constructor(private http: HttpClient) {}
+
+  registerPeople(formData: TRegisterBodyRequest) {
+    return this.http.post<
+      IRegisterPersonReturn | IRegisterPersonReturnBirthday
+    >(`${this.BASE_URL}/people`, formData);
+  }
 }
