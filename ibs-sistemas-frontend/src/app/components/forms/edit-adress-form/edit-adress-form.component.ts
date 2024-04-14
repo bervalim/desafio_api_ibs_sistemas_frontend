2dd5203af2c4437cf1d228257cd8bb07b0c7223c
bervalim <bernardogvalim@gmail.com>
2024-04-14 19:38:25 -0300

    feat/applying effect to edit address Form Component

--- ibs-sistemas-frontend/src/app/components/forms/edit-adress-form/edit-adress-form.component.ts
+++ ibs-sistemas-frontend/src/app/components/forms/edit-adress-form/edit-adress-form.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, effect } from '@angular/core';
 import { AddressService } from '../../../services/address.service';
 import { FormGroup, FormControl, ReactiveFormsModule } from '@angular/forms';
 import { TUpdateAddressBodyRequest } from '../../../interfaces/address.interface';
@@ -11,30 +11,32 @@ import { TUpdateAddressBodyRequest } from '../../../interfaces/address.interface
   styleUrl: './edit-adress-form.component.scss',
 })
 export class EditAdressFormComponent {
-  constructor(private addressService: AddressService) {}
+  constructor(private addressService: AddressService) {
+    effect(() => {
+      this.editAddressForm.setValue({
+        zipCode: this.editingAddress?.zipCode as string,
+        address: this.editingAddress?.address as string,
+        number: this.editingAddress?.number as number,
+        complement: this.editingAddress?.complement as string,
+        neighborhood: this.editingAddress?.neighborhood as string,
+        state: this.editingAddress?.state as string,
+        city: this.editingAddress?.city as string,
+      });
+    });
+  }
 
   get editingAddress() {
     return this.addressService.getEditingAdress();
   }
 
   editAddressForm = new FormGroup({
-    zipCode: new FormControl<string | null>(
-      this.editingAddress?.zipCode as string
-    ),
-    address: new FormControl<string | null>(
-      this.editingAddress?.address as string
-    ),
-    number: new FormControl<number | null>(
-      this.editingAddress?.number as number
-    ),
-    complement: new FormControl<string | null>(
-      this.editingAddress?.complement as string
-    ),
-    neighborhood: new FormControl<string | null>(
-      this.editingAddress?.neighborhood as string
-    ),
-    state: new FormControl<string | null>(this.editingAddress?.state as string),
-    city: new FormControl<string | null>(this.editingAddress?.city as string),
+    zipCode: new FormControl<string | null>(null),
+    address: new FormControl<string | null>(null),
+    number: new FormControl<number | null>(null),
+    complement: new FormControl<string | null>(null),
+    neighborhood: new FormControl<string | null>(null),
+    state: new FormControl<string | null>(null),
+    city: new FormControl<string | null>(null),
   });
 
   setEditingAddressToNull() {
