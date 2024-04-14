2f58e4ed24edbf5b08caa88736eb5ad311041ba2
bervalim <bernardogvalim@gmail.com>
2024-04-14 02:49:42 -0300

    feat/createAddress-request

--- ibs-sistemas-frontend/src/app/api/address.request.ts
+++ ibs-sistemas-frontend/src/app/api/address.request.ts
@@ -1,9 +1,29 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import {
+  IAddress,
+  TCreateAddressBodyRequest,
+} from '../interfaces/address..interface';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AddressRequest {
   constructor(private htpp: HttpClient) {}
+
+  private BASE_URL = 'http://localhost:3000';
+
+  createAddressRequest(data: TCreateAddressBodyRequest) {
+    const token = localStorage.getItem('@TokenIBS');
+
+    if (token) {
+      return this.htpp.post<IAddress>(`${this.BASE_URL}/addresses`, data, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+    } else {
+      return null;
+    }
+  }
 }
