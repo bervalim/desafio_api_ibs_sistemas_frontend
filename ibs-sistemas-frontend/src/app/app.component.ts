fba9216a2a369b72d9d3aca81843e9cc6d0011b5
bervalim <bernardogvalim@gmail.com>
2024-04-15 00:25:35 -0300

    icons-inserted

--- ibs-sistemas-frontend/src/app/app.component.ts
+++ ibs-sistemas-frontend/src/app/app.component.ts
@@ -1,17 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { Component, Type } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { PersonRequest } from './api/person.request';
 import { PersonService } from './services/person.service';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    CommonModule,
-    RouterOutlet, // Módulo para o funcionamen
-  ],
+  imports: [CommonModule, RouterOutlet, MatIconModule],
   providers: [PersonRequest, PersonService], // Serviços utilizados pelo componente
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'], // Corrigido para 'styleUrls' e definido como um array
